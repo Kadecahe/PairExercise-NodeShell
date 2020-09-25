28d8fd53af14fdb4948e675125f698e68e7927d0
Kadecahe <kadecahe@gmail.com>
2020-09-25 15:33:08 -0500

    fixed cat error, added prompt

--- cat.js
+++ cat.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
+const ls = require('./ls');
 
 const cat = function(filePath) {
   fs.readFile(filePath, (err, data) => {
     if (err) throw err;
     console.log(data.toString().trim());
+    process.stdout.write("\nprompt > ");
   })
 }
 
